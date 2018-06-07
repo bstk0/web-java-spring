814bed2acb31a9de9fb51fd2d7742ac79eab911a
bstk0 <rodrigo.bisterco@gmail.com>
2018-06-07 00:44:00 +0000

    lendo oData no listMk

--- src/main/java/org/eclipse/che/examples/ListMkController.java
+++ src/main/java/org/eclipse/che/examples/ListMkController.java
@@ -9,6 +9,13 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.LocalDateTime;
+import org.odata4j.consumer.ODataConsumer;
+import org.odata4j.consumer.ODataConsumers;
+import org.odata4j.jersey.consumer.ODataJerseyConsumer;
+import org.odata4j.core.OEntity;
+import org.odata4j.core.OFuncs;
+import org.odata4j.core.OProperties;
 
 //@Controller
 //public class ListMkController {
@@ -20,7 +27,7 @@ public class ListMkController implements Controller {
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
     {
 
-		List<String> list = getList();
+		List<String> list = getODataCarrosList(); //getList();
 
 		ModelAndView model = new ModelAndView("listMk"); //index
 		model.addObject("lists", list);
@@ -43,5 +50,37 @@ public class ListMkController implements Controller {
 		return list;
 
 	}
+	
+    private List<String> getODataCarrosList() {
+        List<String> list = new ArrayList<String>();
+        
+        // create consumer instance
+        //String serviceUrl = "http://services.odata.org/OData/OData.svc/";
+        String serviceUrl = "http://services.odata.org/V2/OData/OData.svc/";
+        ODataConsumer consumer = ODataConsumers.create(serviceUrl);
+
+        // list category names
+        for (OEntity category : consumer.getEntities("Categories").execute()) {
+            String categoryName = category.getProperty("Name", String.class).getValue();
+            System.out.println("Category name: " + categoryName);
+            list.add(categoryName);
+        }
+
+        
+        /*create consumer instance
+        //String serviceUrl = "http://t846.databoom.space/api1/b846/collections/Carros/"; 
+        String serviceUrl = "http://services.odata.org/OData/OData.svc/";
+        ODataConsumer consumer = ODataConsumers.create(serviceUrl);
+        //ODataConsumer consumer = ODataConsumer.newBuilder(serviceUrl);
+        //ODataConsumer consumer = ODataJerseyConsumer.create(serviceUrl);
+        // list category names
+        for (OEntity category : consumer.getEntities("d").execute()) {
+            String categoryName = category.getProperty("NOME", String.class).getValue();
+            System.out.println("Category name: " + categoryName);
+            list.add(categoryName);
+        }
+        */
+        return list;
+    }
 
 }
